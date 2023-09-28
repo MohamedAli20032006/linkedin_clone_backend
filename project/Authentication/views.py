e562aabb90f5a93602d2f84dab8addade92fb403
محمد علي محمد <you@example.com>
2023-09-28 06:41:01 +0300

    making LoginAPIView class on auth app

--- project/Authentication/views.py
+++ project/Authentication/views.py
@@ -5,3 +5,12 @@ class RegistrationAPIView(CreateAPIView):
     permission_classes = [AllowAny]
     serializer_class= RegistrationSerializer
     
+    
+class LoginAPIView(APIView):
+    permission_classes = [AllowAny]
+    
+    def post(self, request, *args, **kwargs):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
