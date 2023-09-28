48636fc4cf414cd088381a3551f60eace84fc0bc
محمد علي محمد <you@example.com>
2023-09-28 07:14:40 +0300

    config main url file

--- project/project/urls.py
+++ project/project/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('Authentication.urls')),
+    path('profile/', include('Profile.urls')),
+    path('network/', include('Network.urls')),
+    path('post/', include('Post.urls')),
+    path('notification/', include('Notification.urls')),
 ]
