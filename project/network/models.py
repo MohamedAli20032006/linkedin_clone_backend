a246b9fae2c5ca50d51c112040f72e3201812ccc
محمد علي محمد <you@example.com>
2023-09-28 07:20:09 +0300

    making ConnectionRequest class in network app

--- project/network/models.py
+++ project/network/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class ConnectionRequest(models.Model):
+    
+    sender = models.ForeignKey("Profile.Profile", on_delete=models.CASCADE, related_name="sender")
+    reciever = models.ForeignKey("Profile.Profile", on_delete=models.CASCADE, related_name = "reciever")
+    state = models.CharField(max_length=255, choices=CONNECTION_STATES, default="Pending")
+    created_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        unique_together = (('sender', 'reciever'))
+      
+    def __str__(self):
+        return f"{self.sender.full_name} --> {self.reciever.full_name}"
