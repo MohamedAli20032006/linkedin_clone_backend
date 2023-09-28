a4140d0b7e3dbb0ebf4426a9fd480f5427d9b815
محمد علي محمد <you@example.com>
2023-09-28 06:40:16 +0300

    making RegistrationAPIView class on auth app

--- project/Authentication/views.py
+++ project/Authentication/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+class RegistrationAPIView(CreateAPIView):
+    permission_classes = [AllowAny]
+    serializer_class= RegistrationSerializer
+    
