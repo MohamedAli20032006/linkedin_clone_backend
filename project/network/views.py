8ac2f98171ddb52124953e8767bc60e92c2d0ae0
محمد علي محمد <you@example.com>
2023-09-28 07:26:03 +0300

    making ConnectionRequestSendView in views in network app

--- project/network/views.py
+++ project/network/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.generics import *
+from Network.serializers import *
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+from .models import *
+from Profile.models import *
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import get_object_or_404
+from datetime import datetime
 
-# Create your views here.
+class ConnectionRequestSendView(ListCreateAPIView):
+    
+    permission_classes = [IsAuthenticated]
+    serializer_class = ConnectionRequestSerializer
+    
+    def get_queryset(self):
+        profile = get_object_or_404(Profile, user = self.request.user.id)
+        return ConnectionRequest.objects.filter(sender = profile).exclude(state= "Withdrawn")
+
+    def post(self, request, *args, **kwargs):
+        
+        sender_profile = get_object_or_404(Profile, user = self.request.user.id)
+        request.data.update({"sender" : sender_profile.id})
+        try:
+            username = self.request.data['reciever']
+        except KeyError :
+            return super().post(request, *args, **kwargs)
+        
+        reciever_profile = get_object_or_404(Profile, username = username)
+        request.data.update({"reciever" : reciever_profile.id})
+        return super().post(request, *args, **kwargs)
