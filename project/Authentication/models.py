83d07c017ea2edf0eef529512567155c1b379b38
محمد علي محمد <you@example.com>
2023-09-28 06:28:20 +0300

    making emailOTP class in authentication models

--- project/Authentication/models.py
+++ project/Authentication/models.py
@@ -85,3 +85,21 @@ class PhoneOTP(models.Model):
     
     class Meta:
         verbose_name_plural = 'Phone OTPs'
+        
+
+class EmailOTP(models.Model):
+    
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    
+    otp = models.IntegerField(null=True, blank=True)
+    otp_created_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.email}"
+    
+    class Meta:
+        verbose_name_plural = 'Email OTPs'
