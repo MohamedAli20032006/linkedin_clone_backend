35bc95b815d7ae8ff21208698f1186903f73f502
محمد علي محمد <you@example.com>
2023-09-28 06:24:11 +0300

    making usermanager class in authentication models

--- project/Authentication/models.py
+++ project/Authentication/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email,password=None):
+        
+        if not email:
+            raise ValueError('Users must have an email address')
+        
+        if not password:
+            raise ValueError('Users must have a Password')
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+    
+        user = self.create_user(
+            email=email,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
