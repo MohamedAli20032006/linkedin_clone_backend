35abe2335de4c3c9a8043583a8a535097df73c4a
محمد علي محمد <you@example.com>
2023-09-28 06:25:39 +0300

    making user class in authentication models

--- project/Authentication/models.py
+++ project/Authentication/models.py
@@ -28,3 +28,47 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
+#  Custom User Model
+
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f"{self.email}"
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+    
+    @property
+    def is_Active(self):
+        return self.is_active
+    
+    @property
+    def get_tokens(self):
+        refresh = RefreshToken.for_user(self)
+        refresh['email'] = self.email
+        return {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }
