0fcdf1f7a062d32ec336e1d909ac38e4f8d7bd91
محمد علي محمد <you@example.com>
2023-09-29 06:49:54 +0300

    making Notification class in notiication app

--- project/notification/models.py
+++ project/notification/models.py
@@ -4,3 +4,14 @@ from django.db import models
 class NotificationType(models.Model):
     
     type = models.CharField(max_length=100)
+    
+
+class Notification(models.Model):
+    
+    type = models.ForeignKey(NotificationType, on_delete=models.CASCADE)
+    target  = models.ForeignKey(Profile, on_delete = models.CASCADE, related_name = 'received_noti')
+    source  = models.ForeignKey(Profile, on_delete = models.CASCADE, related_name = 'sent_noti')
+    created_at = models.DateTimeField(auto_now = True)
+    seen = models.BooleanField(default = False)
+    action = models.IntegerField()
+    
