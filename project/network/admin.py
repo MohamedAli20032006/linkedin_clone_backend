d3c2b8425f95107113c29eb52405a713e717a159
محمد علي محمد <you@example.com>
2023-09-29 06:45:03 +0300

    customise admin file in network app

--- project/network/admin.py
+++ project/network/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+class ConnectionRequestModelAdmin(admin.ModelAdmin):
+  list_display = ('sender', 'reciever', 'state', 'created_at')
+  
+class FirstConnectionsModelAdmin(admin.ModelAdmin):
+  list_display = ('owner', 'person')
+  
+# class SecondConnectionsModelAdmin(admin.ModelAdmin):
+#   list_display = ('owner', 'person', 'count')
+
+# class ThirdConnectionsModelAdmin(admin.ModelAdmin):
+#   list_display = ('owner', 'person', 'count')
+
+
+admin.site.register(ConnectionRequest, ConnectionRequestModelAdmin)
+admin.site.register(FirstConnections, FirstConnectionsModelAdmin)
+# admin.site.register(SecondConnections, SecondConnectionsModelAdmin)
+# admin.site.register(ThirdConnections, ThirdConnectionsModelAdmin)
+
+admin.site.register(Follow)
